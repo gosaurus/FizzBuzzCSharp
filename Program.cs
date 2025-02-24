914f6fd147d507d7e17a744399c9772526b32853
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-24 22:47:42 +0000

    Outputs 'Bong' (11) and reverses array (17)

--- Program.cs
+++ Program.cs
@@ -4,42 +4,80 @@
     {
         static void Main(string[] args)
             {
-                for(int counter = 1; counter <= 195; counter++)
+                for(int counter = 1; counter <= 255; counter++)
                 {
-                    string output = "";
-                    if (counter % 13 == 0)
+                    string[] outputArray = new string[5];
+                    int[] divisors = {3, 5, 7, 11, 13, 17};
+
+                    foreach (int divisor in divisors)
                     {
-                        output += "Fezz"; 
+                        if (RemainderZero(counter, divisor))
+                        {
+                            outputArray = CreateArray(outputArray, divisor);
+                        }
                     }
-                    if (counter % 3 == 0)
+                    if (outputArray.Contains("Bong"))
                     {
-                        output = "Fizz" + output;
+                        Console.WriteLine($"{counter} Bong ");
                     }
-                    if (counter % 5 == 0)
+                    else if (IsNotNullArray(outputArray)) 
                     {
-                        output += "Buzz";
-                    }
-                    if (counter % 7 == 0)
-                    {
-                        output += "Bang";
-                    }
-                    if (counter % 11 == 0)
-                    {
-                        output = "Bong";
-                    }
-                    if (counter % 17 == 0)
-                    {
-                        output = "Need to reverse array";
-                    }
-                    if (output.Length > 0)
-                    {
-                        Console.Write($"{output} ");
+                        Console.WriteLine($"{counter} {String.Join("", outputArray)} ");
                     }
                     else 
                     {
-                        Console.Write($"{counter} ");
+                        Console.WriteLine($"{counter} ");
                     }
                 }
             }
+        
+        public static bool IsNotNullArray(string[] outputArray)
+        {
+            foreach (var element in outputArray)
+            {
+                if (element != null)
+                    return true;
+            }
+            return false;
+        }
+        
+        public static bool RemainderZero(int counter, int divisor)
+        {
+            if (counter % divisor == 0)
+            return true;
+            else 
+            {
+                return false;
+            }
+        }
+        public static string[] CreateArray(string[] outputArray, int divisor)
+        {
+            if (divisor == 11)
+            {
+                outputArray[0] = "Bong";
+                return outputArray;
+            }
+            if (divisor == 3)
+            {
+                outputArray[1] = "Fizz";
+            }
+            if (divisor == 13)
+            {
+                outputArray[2] = "Fezz"; 
+            }
+            if (divisor == 5)
+            {
+                outputArray[3] = "Buzz";
+            }
+            if (divisor == 7)
+            {
+                outputArray[4] = "Bang";
+            }
+            if (divisor == 17)
+            {
+                Array.Reverse(outputArray);
+            }
+        return outputArray;
+        }
     }
 }
